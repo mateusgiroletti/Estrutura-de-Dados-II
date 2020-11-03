52f06cb48d06400c611dd8ab41d2111ec84a863b
Mateus Giroletti <mateusgiroletti97@gmail.com>
2020-11-03 16:50:06 -0300

    Criando função inserir e imprimindo valores

--- Exercicios/ABB_Biblioteca/exercicio_abb_biblioteca.c
+++ Exercicios/ABB_Biblioteca/exercicio_abb_biblioteca.c
@@ -2,18 +2,50 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-typedef struct sNodo{
-    struct sNodo* esqueda;
-    struct sNodo* direita;
+typedef struct sElemento{
+    struct sElemento* esquerda;
+    struct sElemento* direita;
     int dado;
-} Nodo;
+} Elemento;
 
-void inserir(Nodo*, int);
-int deletar(Nodo*, int);
-Nodo* procurar(Nodo*, int);
+Elemento* inserir(Elemento*, int);
+int deletar(Elemento*, int);
+Elemento* procurar(Elemento*, int);
+void imprimirEmOrdem(Elemento*);
 
+Elemento* inserir(Elemento* elemento, int dado){
+    if(elemento == NULL){
+        elemento = (Elemento*) malloc(sizeof(Elemento));
+        elemento->dado = dado;
+        elemento->direita = NULL;
+        elemento->esquerda = NULL;
+    }else if(dado < elemento->dado){
+        elemento->esquerda = inserir(elemento->esquerda, dado);
+    }else if(dado > elemento->dado){
+        elemento->direita = inserir(elemento->direita, dado);
+    }
+
+    return elemento;
+}
+
+void imprimirEmOrdem(Elemento *elemento){
+	if(elemento!=NULL){
+		imprimirEmOrdem(elemento->esquerda);
+		printf("%i \n",elemento->dado);
+		imprimirEmOrdem(elemento->direita);
+	}
+}
 
 int main(){
+
+    Elemento* arvore = NULL;
+    arvore = inserir(arvore, 10);
+    arvore = inserir(arvore, 5);
+    arvore = inserir(arvore, 1);
+    arvore = inserir(arvore, 2);
+
+    imprimirEmOrdem(arvore);
+
 
     
 }
